bf2fc791e0fc5f636d8a98c8e01346956fb1ec19
JessySeo9955 <jessy.seo.9955@gmail.com>
2025-06-29 21:16:19 -0400

    plugin code update

--- application/plugins/02.init-loading.ts
+++ application/plugins/02.init-loading.ts
@@ -4,17 +4,13 @@ export default defineNuxtPlugin({
     parallel: true,
     setup() {
         const { start, finish } = useGlobalLoading();
-        onNuxtReady(finish);
+
+        onNuxtReady(() => finish());
 
         const router = useRouter();
-        router.beforeEach(() => {
-            start();
-        });
+        router.beforeEach(() => start());
         router.afterEach(() => {
-            nextTick(() => {
-                // Waits until the DOM is updated, then hides spinner
-                finish();
-            });
+            nextTick(() => finish());
         });
     },
 });
